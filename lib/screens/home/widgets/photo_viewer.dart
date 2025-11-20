b34156f40d8192cbf85b3b4f7ddf79e53bd4bf4a
Echa Imuts <vanda@vandas-MacBook-Pro.local>
2025-11-20 10:51:50 +0700

    creating photo viewer widget

--- lib/screens/home/widgets/photo_viewer.dart
+++ lib/screens/home/widgets/photo_viewer.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:attendance_app/services/storage_services.dart';
 import 'package:flutter/material.dart';
 
 class PhotoViewer extends StatelessWidget {
@@ -32,6 +35,132 @@ class PhotoViewer extends StatelessWidget {
         ),
       ),
     );
-  };
+  }
+
+  final storageService = StorageServices();
+
+  return FutureBuilder<String?>(
+    future: storageService.getPhotoBase64(photokey!),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Container(
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Center(child: CircularProgressIndicator()),
+        );
+      }
+
+      if (snapshot.hasError || !snapshot.hasData || snapshot.data == null) {
+        return Container(
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.error_outline, color: Colors.red[400], size: 40),
+                SizedBox(height: 8),
+                Text(
+                  'Failed to load photo',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }
+
+      try {
+        final Uint8List bytes = base64Decode(snapshot.data!); // Uint8List adalah array of bytes
+
+        return GestureDetector(
+          onTap: () => _showFullImage(context, bytes, label),
+          child: Container(
+            height: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(
+                image: MemoryImage(bytes),
+                fit: BoxFit.cover
+                )
+            ),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 4),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(8),
+                  bottomRight: Radius.circular(8)
+                )
+              ),
+              child: Text(
+                label,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+          ),
+        );
+      } catch (e) {
+        return Container(
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Center(
+            child: Text(
+              'Invalid Image data',
+              style: TextStyle(
+                color: Colors.grey[600]
+              ),
+            ),
+          ),
+        );
+      }
+    },
+  );
+  }
+
+   void _showFullImage(BuildContext context, Uint8List bytes, String title) {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppBar(
+              title: Text(title),
+              automaticallyImplyLeading: false,
+              actions: [
+                IconButton(
+                 icon:Icon(Icons.close),
+                onPressed: () => Navigator.pop(context),
+                )
+              ],
+            ),
+            InteractiveViewer(
+              child: Image.memory(bytes),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
